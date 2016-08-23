5754c1e7e20eba69e93d96f2589fba40911c7100
Yuliya Nykytenko <yuliya.nykytenko@gmail.com>
2016-08-23 21:29:26 +0300

    Added full alpha test.

--- Palindrom.py
+++ Palindrom.py
@@ -5,4 +5,5 @@ from string import ascii_lowercase as az
 def check(text):
 	return set(text.lower()).issuperset(set(az))
 
-print(check('abc'))
+print(check('abc') == False)
+print (check('abcdefghijklmnopqrstuvwxyz'))
